5705f3808b612e72671f8054f849bd6b8281ded3
nepalialberta <nepalialberta@gmail.com>
2023-03-07 21:41:04 +0000

    Done

--- src/main/java/AdvancedBooleanLogic.java
+++ src/main/java/AdvancedBooleanLogic.java
@@ -16,7 +16,11 @@ public class AdvancedBooleanLogic {
      * @return false if b1 and b2 are both true. Otherwise, return true.
      */
     public boolean returnNotBoth(boolean b1, boolean b2){
-        return false;
+
+        if(b1&&b2==true){
+            return false;
+        }
+        return true;
     }
     /**
      * b1 OR b2, BUT NOT BOTH
@@ -25,7 +29,11 @@ public class AdvancedBooleanLogic {
      * @return true if either b1 or b2 is true, but false if they are both true or both false.
      */
     public boolean returnExclusiveOr(boolean b1, boolean b2){
-        return false;
+
+        if((b1&&b2)||(!b1&&!b2)){
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -35,6 +43,10 @@ public class AdvancedBooleanLogic {
      * @return true if neither b1 nor b2 are true. Return false if either b1 or b2 is true.
      */
     public boolean returnNeitherNor(boolean b1, boolean b2){
+
+        if(!(b1||b2==true)){
+            return true;
+        }
         return false;
     }
 }
